b78df3962d9b28f6ed5ca321a2949334295743c4
mradisa <114479689+mradisa@users.noreply.github.com>
2024-05-16 15:58:48 +0100

    Update runner-install.sh

--- runner-install.sh
+++ runner-install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Installing make-profiler
-sudo apt install -y python3-pip graphviz gawk
-pip3 install slack slackclient
-sudo pip3 install https://github.com/konturio/make-profiler/archive/master.zip
-sudo apt-get install -y cmake
-sudo apt install -y unzip
-sudo apt-get -y install python3-boto3 python3-botocore # amazon.aws.aws_s3
-sudo apt-get install -y cron
+apt install -y python3-pip graphviz gawk
+
+pip3 install https://github.com/konturio/make-profiler/archive/master.zip
+apt-get install -y cmake
+apt install -y unzip
+apt-get -y install python3-boto3 python3-botocore # amazon.aws.aws_s3
+apt-get install -y cron
 
 # python-is-python3 is a convenient way to set up a symlink for /usr/bin/python, pointing to /usr/bin/python3
-sudo apt install -y python-is-python3
+apt install -y python-is-python3
 
 # osm
 pip3 install osmnx geopandas pandas pathlib
